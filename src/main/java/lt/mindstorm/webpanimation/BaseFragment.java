27b1f9bac25eb76a0caad19ba471bd520b0a5627
jagas <mindaugas@weissmancooper.com>
2016-09-27 13:50:57 +0300

    base fragment start on activity result uncomment

--- src/main/java/lt/mindstorm/webpanimation/BaseFragment.java
+++ src/main/java/lt/mindstorm/webpanimation/BaseFragment.java
@@ -1,32 +1,36 @@
 package lt.mindstorm.webpanimation;
 
 import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 
 public class BaseFragment extends Fragment {
 
-//    public void startActivityForResult(Intent intent, int requestCode, Bundle options) {
-//        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
-//            ActivityCompat.startActivity(getActivity(), intent, options);
-//        } else {
-//            int code = requestCode;
-//            if (requestCode != -1) {
-//                try {
-//                    Field index = Fragment.class.getDeclaredField("mIndex");
-//                    index.setAccessible(true);
-//                    code = ((index.getInt(this) + 1) << 16) + (requestCode & 0xffff); //NOPMD
-//                } catch (NoSuchFieldException e) {
-//                    throw new RuntimeException(e);
-//                } catch (IllegalAccessException e) {
-//                    throw new RuntimeException(e);
-//                }
-//            }
-//            ActivityCompat.startActivityForResult(getActivity(), intent, code, options);
-//        }
-//    }
+    public void startActivityForResult(Intent intent, int requestCode, Bundle options) {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN) {
+            ActivityCompat.startActivity(getActivity(), intent, options);
+        } else {
+            int code = requestCode;
+            if (requestCode != -1) {
+                try {
+                    Field index = Fragment.class.getDeclaredField("mIndex");
+                    index.setAccessible(true);
+                    code = ((index.getInt(this) + 1) << 16) + (requestCode & 0xffff); //NOPMD
+                } catch (NoSuchFieldException e) {
+                    throw new RuntimeException(e);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            ActivityCompat.startActivityForResult(getActivity(), intent, code, options);
+        }
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
